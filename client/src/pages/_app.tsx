b2226940115937d9026d14e338be101fc470e4ba
jsb-dev <jsb-dev@outlook.com>
2023-11-28 17:05:00 +1100

    Test with global scss import repositioning

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import type { AppProps } from 'next/app';
 import store from '@/redux/store';
 import 'normalize.css'
-import '@/styles/globals.scss';
 
 const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN!;
 const AUTH0_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!;
@@ -25,5 +24,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     </Auth0Provider>
   );
 }
+
+import '@/styles/globals.scss';
 
 export default MyApp;
